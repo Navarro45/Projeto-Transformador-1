a333c7c918bd0729a18f212659cf1a6110d6a68d
Gabriel Martins <ga.a.martins.a@gmail.com>
2026-05-18 17:09:24 -0300

    imagens

--- data/data_module.py
+++ data/data_module.py
@@ -1,6 +1,7 @@
 from torchvision import datasets, transforms
+from torch.utils.data import (DataLoader, random_split,Subset)
 
-from torch.utils.data import (DataLoader,random_split)
+import random
 
 
 class DataModule:
@@ -11,54 +12,152 @@ class DataModule:
         test_dir,
         img_size=224,
         batch_size=32,
-        validation_split=0.2
+        validation_split=0.2,
+
+        max_train_images=None,
+        max_validation_images=None,
+        max_test_images=None,
+
+        seed=42
     ):
 
+        random.seed(seed)
+
         self.transform = transforms.Compose([
-            transforms.Resize((img_size, img_size)),
+
+            transforms.Resize(
+                (img_size, img_size)
+            ),
+
             transforms.ToTensor()
         ])
+
+        # =========================
+        # DATASETS
+        # =========================
 
         full_train_dataset = datasets.ImageFolder(
             train_dir,
             transform=self.transform
         )
 
-        self.test_dataset = datasets.ImageFolder(
+        full_test_dataset = datasets.ImageFolder(
             test_dir,
             transform=self.transform
         )
+
+        # =========================
+        # SPLIT
+        # =========================
 
         train_size = int(
             (1 - validation_split)
             * len(full_train_dataset)
         )
 
-        val_size = len(full_train_dataset) - train_size
+        val_size = (
+            len(full_train_dataset)
+            - train_size
+        )
 
-        self.train_dataset, self.validation_dataset = random_split(
+        train_dataset, validation_dataset = random_split(
             full_train_dataset,
             [train_size, val_size]
         )
 
+        # =========================
+        # LIMIT TRAIN
+        # =========================
+
+        if max_train_images is not None:
+
+            indices = random.sample(
+                range(len(train_dataset)),
+                min(
+                    max_train_images,
+                    len(train_dataset)
+                )
+            )
+
+            train_dataset = Subset(
+                train_dataset,
+                indices
+            )
+
+        # =========================
+        # LIMIT VALIDATION
+        # =========================
+
+        if max_validation_images is not None:
+
+            indices = random.sample(
+                range(len(validation_dataset)),
+                min(
+                    max_validation_images,
+                    len(validation_dataset)
+                )
+            )
+
+            validation_dataset = Subset(
+                validation_dataset,
+                indices
+            )
+
+        # =========================
+        # LIMIT TEST
+        # =========================
+
+        if max_test_images is not None:
+
+            indices = random.sample(
+                range(len(full_test_dataset)),
+                min(
+                    max_test_images,
+                    len(full_test_dataset)
+                )
+            )
+
+            full_test_dataset = Subset(
+                full_test_dataset,
+                indices
+            )
+
+        # =========================
+        # LOADERS
+        # =========================
+
         self.train_loader = DataLoader(
-            self.train_dataset,
+            train_dataset,
             batch_size=batch_size,
             shuffle=True
         )
 
         self.validation_loader = DataLoader(
-            self.validation_dataset,
+            validation_dataset,
             batch_size=batch_size,
             shuffle=False
         )
 
         self.test_loader = DataLoader(
-            self.test_dataset,
+            full_test_dataset,
             batch_size=batch_size,
             shuffle=False
         )
 
         self.class_names = (
             full_train_dataset.classes
+        )
+
+        print(
+            f"Train images: {len(train_dataset)}"
+        )
+
+        print(
+            f"Validation images: "
+            f"{len(validation_dataset)}"
+        )
+
+        print(
+            f"Test images: "
+            f"{len(full_test_dataset)}"
         )
